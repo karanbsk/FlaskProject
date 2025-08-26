03f54f5ec94c93722837138131e01cea033826a4
Karan Besaudan <karanbsk@gmail.com>
2025-08-26 22:34:44 +0530

    Added separate dev and prod docker-compose setups

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 # app/__init__.py
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.blueprints.main import main
@@ -7,10 +8,25 @@ from config import DevelopmentConfig, ProductionConfig, TestingConfig
 #initialize db
 db = SQLAlchemy()
 
-def create_app(config_class=DevelopmentConfig):
+# Map string to config class
+CONFIG_MAP = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig
+}
+
+def create_app():
+        
+    # Load config based on environment variable
+    config_name = os.getenv("APP_CONFIG", "development").lower()
+    config_class = CONFIG_MAP.get(config_name, DevelopmentConfig)
+    
+    #Initialize Flask App
     app = Flask(__name__,template_folder='templates', static_folder='static')
     app.config.from_object(config_class)
-
+    
+    app.debug = True if config_name == "development" else False
+    
     #initialize db with app
     db.init_app(app)
     
--- create_db.py
+++ create_db.py
@@ -1,9 +1,10 @@
 # create_db.py
+import os
 from app import create_app, db
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
-    print("Database created successfully!")
-
+    config_name = os.getenv("APP_CONFIG", "development").lower()
+    print(f"Database created for {config_name.upper()} successfully!")
