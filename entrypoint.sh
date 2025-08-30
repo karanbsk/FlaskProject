5b0359fce004fe17db88b6ecc93a8a9a92676f7a
Karan Besaudan <karanbsk@gmail.com>
2025-08-31 00:11:07 +0530

    made entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
-set -e  # Exit immediately if a command fails
+set -e
 
-echo "🚀 Starting Flask App in Production Mode..."
-
-# Run database migrations
-echo "📦 Applying database migrations..."
+echo "Running database migrations..."
 flask db upgrade
 
-# Start the Flask app using Gunicorn
-echo "✅ Starting Gunicorn server..."
-exec gunicorn --bind 0.0.0.0:8000 "app:create_app()"
+echo "Starting Gunicorn server..."
+exec "$@"
